cb084e01bec0cfd287bcf5e4e9093c1d6937c5f8
walter.hwang <lyvius2@naver.com>
2018-01-08 18:02:47 +0900

    로그인 구현을 위한 기초 코드

---
+++ src/main/java/com/newspaper/reader/ReaderWebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.newspaper.reader;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ReaderWebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+        // super.configure(http);
+    }
+
+    /**
+     * SpringBoot 에 Security Login 적용 실습을 위함 임시 메서드
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+}
